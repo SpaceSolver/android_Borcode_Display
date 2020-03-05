e3275f4a7ec7638414ceeab05678cf8952a730e2
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-03-05 15:38:29 +0900

    ・複数フォーマット選択 ・例外ログにトレースルート追加

--- app/src/main/java/SpaceSolver/co/MainActivity.java
+++ app/src/main/java/SpaceSolver/co/MainActivity.java
@@ -7,14 +7,19 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.oned.CodaBarWriter;
+import com.google.zxing.oned.Code128Writer;
+import com.google.zxing.oned.ITFWriter;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,11 +28,48 @@ public class MainActivity extends AppCompatActivity
     int    width      = 400;      //作成するバーコードの幅
     int    height     = 200;      //作成するバーコードの高さ
 
+    // 選択肢
+    private String[] spinnerItems = {"CODABAR", "CODE_128", "ITF"};
+    public TextView SelectItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.SelectFormat);
+
+        // ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinnerItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // spinner に adapter をセット
+        spinner.setAdapter(adapter);
+
+        try
+        {
+            // リスナーを登録
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+            {
+                //　アイテムが選択された時
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+                {
+                    Spinner spinner = (Spinner) parent;
+                    String item = (String) spinner.getSelectedItem();
+                }
+
+                //　アイテムが選択されなかった
+                public void onNothingSelected(AdapterView<?> parent)
+                {
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            Log.e("ERROR","例外発生" + e);
+        }
     }
 
     public void OnClickAddButton(View view)
@@ -41,8 +83,6 @@ public class MainActivity extends AppCompatActivity
             // 入力データをバーコードに変換する対象データに代入
             targetData = editText.getText().toString();
 
-            // バーコードフォーマット種別を選択
-
             // バーコード表示処理
             pix = DisplayBarcode();
             DisplayBitmapImage(pix);
@@ -53,7 +93,7 @@ public class MainActivity extends AppCompatActivity
         }
         catch (Exception e)
         {
-            Log.e("ERROR","例外発生");
+            Log.e("ERROR","例外発生" + e);
         }
     }
 
@@ -72,21 +112,40 @@ public class MainActivity extends AppCompatActivity
         }
         catch (Exception e)
         {
-            Log.e("ERROR","例外発生");
+            Log.e("ERROR","例外発生" + e);
         }
     }
 
-
     public int[] DisplayBarcode()
     {
-        // CODABAR規格用のデータ変換クラスをインスタンス化する
-        CodaBarWriter writer = new CodaBarWriter();
         int[] pixels = null;
+        Spinner spinner = findViewById(R.id.SelectFormat);
 
-        try {
-            // 対象データを変換する
-            BitMatrix bitMatrix = writer.encode(targetData, BarcodeFormat.CODABAR, width, height);
+        try
+        {
+            BitMatrix bitMatrix = null;
 
+            if("CODABAR" == (String) spinner.getSelectedItem())
+            {
+                CodaBarWriter writer = new CodaBarWriter();
+                bitMatrix = writer.encode(targetData, BarcodeFormat.CODABAR, width, height);
+            }
+            else if("CODE_128" == (String) spinner.getSelectedItem())
+            {
+                Code128Writer writer = new Code128Writer();
+                bitMatrix = writer.encode(targetData, BarcodeFormat.CODE_128, width, height);
+            }
+            else if("TIF" == (String) spinner.getSelectedItem())
+            {
+                ITFWriter writer = new ITFWriter();
+                bitMatrix = writer.encode(targetData, BarcodeFormat.ITF, width, height);
+            }
+            else
+            {
+                CodaBarWriter writer = new CodaBarWriter();
+                bitMatrix = writer.encode(targetData, BarcodeFormat.CODABAR, width, height);
+            }
+            
             // BitMatrixのデータが「true」の時は「黒」を設定し、「false」の時は「白」を設定する
             pixels = new int[width * height];
             for (int y = 0; y < height; y++)
@@ -100,7 +159,7 @@ public class MainActivity extends AppCompatActivity
         }
         catch (WriterException e)
         {
-            Log.e("ERROR","例外発生");
+            Log.e("ERROR","例外発生" + e);
         }
         return pixels;
     }
@@ -123,7 +182,7 @@ public class MainActivity extends AppCompatActivity
         }
         catch (Exception e)
         {
-            Log.e("ERROR","例外発生");
+            Log.e("ERROR","例外発生" + e);
         }
     }
 }
